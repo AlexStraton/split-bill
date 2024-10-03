90a77999567ba4cac618d15df30eaa6cf6a491eb
Alex Straton <alexandrastraton1@gmail.com>
2024-10-03 15:46:31 +0100

    controlled the inputs and dropdown in the split bill component

--- src/components/SplitBill.js
+++ src/components/SplitBill.js
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function SplitBill({ selectedFriend }) {
+  const [billValue, setBillValue] = useState("");
+  const [yourExpense, setYourExpense] = useState("");
+  const [who, setWho] = useState("you");
+  const paidByFriend = billValue ? billValue - yourExpense : "";
+
   return (
     <form className='form-split-bill'>
       <div>Split a bill with {selectedFriend.name}</div>
       <label>Bill Value : </label>
-      <input type='text' />
+      <input
+        value={billValue}
+        onChange={(e) => setBillValue(Number(e.target.value))}
+        type='text'
+      />
       <label>Your expense: </label>
-      <input type='text' />
+      <input
+        value={yourExpense}
+        onChange={(e) =>
+          setYourExpense(
+            Number(e.target.value) > billValue
+              ? yourExpense
+              : Number(e.target.value)
+          )
+        }
+        type='text'
+      />
       <label>{selectedFriend.name}'s' expense: </label>
-      <input type='text' disabled />
+      <input type='text' disabled value={paidByFriend} />
       <label>Who is paying the bill? </label>
-      <select name='who' id='select-payer'>
+      <select
+        name='who'
+        id='select-payer'
+        onChange={(e) => setWho(e.target.value)}>
         <option value='you'>You</option>
         <option value='friend'>{selectedFriend.name}</option>
       </select>
